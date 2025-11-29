66b0f76fb4e80f5ab677336c3c30c4e63d4625cf
Rahidul Islam <rahiseli@outlook.com>
2025-11-29 11:36:34 +0600

    agents page is done and fixed  static images issue

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, AboutView, PropertiesView, PropertyDetailView
+from .views import HomeView, AboutView, PropertiesView, PropertyDetailView, AgentsView
 
 app_name = 'core'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
     path('properties/', PropertiesView.as_view(), name='properties'),
     path('properties/details/', PropertyDetailView.as_view(), name='property_detail'),
+    path('agents/', AgentsView.as_view(), name='agents'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "core/index.html"
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
+
 class AboutView(TemplateView):
     template_name = "core/about.html"
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
+
 class PropertiesView(TemplateView):
     template_name = "core/properties.html"
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
+
 class PropertyDetailView(TemplateView):
     template_name = "core/property_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class AgentsView(TemplateView):
+    template_name = "core/agents.html"
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
